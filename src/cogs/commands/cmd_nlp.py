914133b4dc72279a51f3da20c484d73dae212358
danczw <dev.dc@pm.me>
2023-07-21 17:51:45 +0200

    add timeout wrapper to Open AI call

--- src/cogs/commands/cmd_nlp.py
+++ src/cogs/commands/cmd_nlp.py
@@ -1,4 +1,5 @@
 import logging
+from concurrent.futures import ThreadPoolExecutor, TimeoutError
 
 import discord
 import openai
@@ -83,29 +84,77 @@ class NlpCommands(commands.Cog):
             timeframe=self.config_params["chat_history_timeframe"],
             connection=chat_conn,
         )
-
-        # create message including chat history
+        # filter context length
+        chat_history = chat_history[-self.config_params["chat_history_ctx_length"] :]
+        logger.debug(f"Chat history for {ctx.user}: {len(chat_history)} messages used as context.")
+        # create message context
         message_context = [{"role": hist[0], "content": hist[1]} for hist in chat_history]
-        # use GPT3 to create an answer
-        openai.api_key = self.KEYS["OPENAI_API_KEY"]
-        response_oai = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=message_context,
-            max_tokens=800,
-            n=1,
-            # temperature=1,
-            # frequency_penalty=1.1
-            request_timeout=90,  # experimental, undocumented parameter
-        )
-        # extract response content
-        response = response_oai.choices[0].message.content  # type: ignore
 
-        # add response to chat db
-        add_message_to_chat_db(
-            username=str(ctx.user),
-            message=response,
-            role="assistant",
-            connection=chat_conn,
+        # use Open AI'S gpt to create an answer, wrapped in a timeout
+        openai.api_key = self.KEYS["OPENAI_API_KEY"]
+        response_oai = self.helper_oai_call(
+            message_context=message_context,
+            model=self.config_params["oai_model"],
+            max_tokens=self.config_params["oai_max_tokens"],
+            timeout=self.config_params["oai_timeout"],
         )
+
+        # check if timeout
+        if response_oai is None:
+            return "OpenAI's robots seem to be very tired. :zzz: Please try again later."
+        else:
+            # extract response content
+            response = response_oai.choices[0].message.content  # type: ignore
+
+            # add response to chat db
+            add_message_to_chat_db(
+                username=str(ctx.user),
+                message=response,
+                role="assistant",
+                connection=chat_conn,
+            )
 
         return response
+
+    def helper_oai_call(
+        self,
+        message_context: list,
+        model: str = "gpt-4",
+        max_tokens: int = 800,
+        n: int = 1,
+        temperature: float = 1,
+        frequency_penalty: float = 0,
+        timeout: int = 60,
+    ):
+        """helper function to call openai api with client-side timeout
+
+        Args:
+            message_context (list): list of dicts with message context
+            model (str, optional): openai model to use. Defaults to "gpt-4".
+            max_tokens (int, optional): max tokens to use. Defaults to 800.
+            n (int, optional): number of responses to return. Defaults to 1.
+            temperature (float, optional): temperature to use. Defaults to 1.
+            frequency_penalty (float, optional): frequency penalty to use. Defaults to 0.
+            timeout (int, optional): timeout in seconds. Defaults to 60.
+
+        Returns:
+            None if timeout, else openai response
+        """
+
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            future = executor.submit(
+                openai.ChatCompletion.create,
+                messages=message_context,
+                model=model,
+                max_tokens=max_tokens,
+                n=n,
+                temperature=temperature,
+                frequency_penalty=frequency_penalty,
+            )
+
+            try:
+                response_oai = future.result(timeout=timeout)  # timeout after 5 seconds
+                return response_oai
+            except TimeoutError:
+                logger.error("TimeoutError: OpenAI API call timed out.")
+                return None
